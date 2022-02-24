8da6b9d0a5d8793b39af7b707093dc7b4ef78480
Lords <92923519+iiX0Lords@users.noreply.github.com>
2022-02-24 16:15:30 +1100

    Create Text.lua

---
+++ Text.lua
@@ -0,0 +1,12 @@
+--- Normal Text ---
+local NormalText = {"You feel like someones watching you...","You suddenly feel cold...","Your stomach aches..","A chill runs down your spine..","The voices are trying to warn you about something..","You feel drained..","The whispers... what are they saying?","You can see figures in the corner of your eye..","Did you miss me?","It is time.."}
+
+--- Hahaha funny ---
+local FunnyText = {"'What's in the dark, always come to light.' - Dhar Mann","'I have cum in my ass' - Callum Smith","So you see","Dhar mann porn","Steve from Dhar mann is the best actor","wow","hello","hunter chode","Redwood Prison Gang!"}
+
+
+if _G.CurrentTable == "Normal" then
+    return NormalText
+    else
+    return FunnyText
+ end
