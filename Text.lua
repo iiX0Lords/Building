68d8f0fc783a8a93b7089619b3c77696a933d9e2
Lords <92923519+iiX0Lords@users.noreply.github.com>
2022-02-24 21:58:40 +1100

    Update Text.lua

--- Text.lua
+++ Text.lua
@@ -1,5 +1,5 @@
 --- Normal Text ---
-local NormalText = {"You feel like someones watching you...","You suddenly feel cold...","Your stomach aches..","A chill runs down your spine..","The voices are trying to warn you about something..","You feel drained..","The whispers... what are they saying?","You can see figures in the corner of your eye..","Did you miss me?","It is time.."}
+local NormalText = {"You feel num..","You feel like someones watching you...","You suddenly feel cold...","Your stomach aches..","A chill runs down your spine..","You feel drained..","You can see figures in the corner of your eye.."}
 
 --- Hahaha funny ---
 local FunnyText = {"'If he was food i would say yes' - Callum chodes crush","My hairs kinda sticky i wonder why?","I have cum dripping into my mouth","Let's have sex!","Can i rub your clit?","You a squirter?","Im bleeding from my uterus","'I love tentacle hentai' - Hunter chode","Synapse X gang is going to rape you","You on your period?","Jelos kids are gay","I just shit my panties","'I love anime girls' - Johnonthon Susserate","'What's in the dark, always come to light.' - Dhar Mann","'I have cum in my ass' - Callum Smith","So you see","Dhar mann porn","Steve from Dhar mann is the best actor","wow","hello","hunter chode","Redwood Prison Gang!"}
